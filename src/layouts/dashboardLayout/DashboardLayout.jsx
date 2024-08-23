397b521b829f7c0e78da3ffc74ca8e991b24646d
Philip-Machar <philipmachar788@gmail.com>
2024-08-23 12:07:58 +0300

    Initial commit

--- src/layouts/dashboardLayout/DashboardLayout.jsx
+++ src/layouts/dashboardLayout/DashboardLayout.jsx
@@ -1,9 +1,15 @@
+import { Outlet } from 'react-router-dom';
 import './dashboardLayout.css';
 
 const DashboardLayout = () => {
-    return (
-        <div>Dashboard layout</div>
-    )
+  return (
+    <div className="dashboardLayout">
+      <div className="menu">MENU</div>
+      <div className="content">
+        <Outlet />
+      </div>
+    </div>
+  )
 }
 
 export default DashboardLayout;
