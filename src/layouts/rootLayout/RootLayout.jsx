e8e2d09b17a8bbd4cd32694828d1d0bf1c07eeb8
Philip-Machar <philipmachar788@gmail.com>
2024-08-24 07:56:30 +0300

    Authentication using clerk

--- src/layouts/rootLayout/RootLayout.jsx
+++ src/layouts/rootLayout/RootLayout.jsx
@@ -1,21 +1,38 @@
 import { Outlet } from 'react-router-dom';
 import { Link } from 'react-router-dom';
 import './rootLayout.css';
+import { ClerkProvider } from '@clerk/clerk-react'
+import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/clerk-react";
+
+const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
+
+if (!PUBLISHABLE_KEY) {
+  throw new Error("Missing Publishable Key")
+}
 
 const RootLayout = () => {
   return (
-    <div className="rootLayout">
-      <header>
-        <Link to='/' className="logo">
-          <img src="/logo.png" />
-          <span>Machar AI</span>
-        </Link>
-        <div>user</div>
-      </header>
-      <main>
-        <Outlet />
-      </main>
-    </div>
+    <ClerkProvider publishableKey={PUBLISHABLE_KEY} afterSignOutUrl="/">
+      <div className="rootLayout">
+        <header>
+          <Link to='/' className="logo">
+            <img src="/logo.png" />
+            <span>Machar AI</span>
+          </Link>
+          <div className="user">
+            <SignedOut>
+              <SignInButton />
+            </SignedOut>
+            <SignedIn>
+              <UserButton />
+            </SignedIn>
+          </div>
+        </header>
+        <main>
+          <Outlet />
+        </main>
+      </div>
+    </ClerkProvider>
   )
 }
 
