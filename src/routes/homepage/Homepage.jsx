f2f040a300038436d767ed8ac4fabe54caba2ef8
Philip-Machar <philipmachar788@gmail.com>
2024-08-24 16:15:58 +0300

    Home page design

--- src/routes/homepage/Homepage.jsx
+++ src/routes/homepage/Homepage.jsx
@@ -4,6 +4,7 @@ import { Link } from 'react-router-dom';
 const Homepage = () => {
   return (
     <div className='homepage'>
+      <img className="orbital" src="/orbital.png" alt="orbital image" />
       <div className="left">
         <h1>MACHAR AI</h1>
         <h2>Super charge your creativity and productivity</h2>
@@ -12,7 +13,14 @@ const Homepage = () => {
         </h3>
         <Link to="/dashboard">Get Started</Link>
       </div>
-      <div className="right">fldsfjldkfjdslkfldsk</div>
+      <div className="right">
+        <div className="img-container">
+          <div className="bg-container">
+            <div className="bg"></div>
+          </div>
+          <img className="bot" src="/bot.png" alt="bot image" />
+        </div>
+      </div>
     </div>
   )
 }
