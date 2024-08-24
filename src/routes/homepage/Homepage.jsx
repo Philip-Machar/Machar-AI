a2b33e0cf7d911b1437ca3ee5adf9b0c5b618285
Philip-Machar <philipmachar788@gmail.com>
2024-08-24 14:06:52 +0300

    Homepage design

--- src/routes/homepage/Homepage.jsx
+++ src/routes/homepage/Homepage.jsx
@@ -2,11 +2,19 @@ import './homepage.css';
 import { Link } from 'react-router-dom';
 
 const Homepage = () => {
-    return (
-        <div className='homepage'>
-            <Link to="/dashboard">Dashboard</Link>
-        </div>
-    )
+  return (
+    <div className='homepage'>
+      <div className="left">
+        <h1>MACHAR AI</h1>
+        <h2>Super charge your creativity and productivity</h2>
+        <h3>
+          Machar AI boosts your creativity and productivity with its intelligent, quick, and reliable support.
+        </h3>
+        <Link to="/dashboard">Get Started</Link>
+      </div>
+      <div className="right">fldsfjldkfjdslkfldsk</div>
+    </div>
+  )
 }
 
 export default Homepage;
