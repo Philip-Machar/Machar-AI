93fbc67eb46e545c0172245fb162c19b1a9efd6e
Philip-Machar <philipmachar788@gmail.com>
2024-08-29 08:15:28 +0300

    Chat page

---
+++ src/components/newPrompt/NewPrompt.jsx
@@ -0,0 +1,9 @@
+import './newPrompt.css';
+
+const NewPrompt = () => {
+  return (
+    <div>new prompt</div>
+  )
+}
+
+export default NewPrompt;
--- src/routes/chatPage/ChatPage.jsx
+++ src/routes/chatPage/ChatPage.jsx
@@ -1,9 +1,39 @@
+import { useEffect, useRef } from 'react';
 import './chatPage.css';
+import NewPrompt from '../../components/newPrompt/NewPrompt';
 
 const ChatPage = () => {
-    return (
-        <div className='chatPage'>chatPage</div>
-    )
+  const endRef = useRef();
+
+  useEffect(() => {
+    endRef.current.scrollIntoView({behavior: 'smooth'});
+  }, []);
+  return (
+    <div className="chatPage">
+      <div className="wrapper">
+        <div className="chat">
+          <div className="message">Test message from AI</div>
+          <div className="message user">Test message from user ;slfk;dsf f ds; fdsf; kf ds;afkdsakdslf kd sf kdslf;k;df kds;fk dsfk dlf kds;fk dls fkds;fk ds;lfk ds; f</div>
+          <div className="message">Test message from AI</div>
+          <div className="message user">Test message from user</div>
+          <div className="message">Test message from AI</div>
+          <div className="message user">Test message from user</div>
+          <div className="message">Test message from AI</div>
+          <div className="message user">Test message from user</div>
+          <div className="message">Test message from AI</div>
+          <div className="message user">Test message from user</div>
+          <div className="message">Test message from AI</div>
+          <div className="message user">Test message from user</div>
+          <div className="message">Test message from AI</div>
+          <div className="message user">Test message from user</div>
+          <div className="message">Test message from AI</div>
+          <div className="message user">Test message from user</div>
+          <NewPrompt />
+          <div ref={endRef} />
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default ChatPage;
