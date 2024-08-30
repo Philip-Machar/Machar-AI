3726b527f5a16bc17871611294f492d7195eb34a
Philip-Machar <philipmachar788@gmail.com>
2024-08-30 19:36:07 +0300

    test finitshed

--- src/routes/signInPage/SignInPage.jsx
+++ src/routes/signInPage/SignInPage.jsx
@@ -1,27 +1,12 @@
 import './signInPage.css';
-import { SignIn, useClerk } from '@clerk/clerk-react';
-import { useEffect } from 'react';
-import { useNavigate } from 'react-router-dom';
+import { SignIn } from '@clerk/clerk-react';
 
 const SignInPage = () => {
-  const { redirectToSignIn } = useClerk();
-  const navigate = useNavigate();
-
-  useEffect(() => {
-    if (window.location.search.includes('__clerk_handshake')) {
-      navigate('/Machar-AI/dashboard', { replace: true });
-    }
-  }, [navigate]);
-
   return (
     <div className='signInPage'>
-      <SignIn
-        path="/Machar-AI/sign-in"
-        signUpUrl="/Machar-AI/sign-up"
-        afterSignInUrl="/Machar-AI/dashboard"
-      />
+      <SignIn path="/Machar-AI/sign-in" signUpUrl="/Machar-AI/sign-up" forceRedirectUrl="/Machar-AI/dashboard/" />
     </div>
-  );
+  )
 }
 
 export default SignInPage;
