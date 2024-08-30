f5ea1437762b5ee67c96524cbfb007592345bcca
Philip-Machar <philipmachar788@gmail.com>
2024-08-30 13:21:55 +0300

    debug

--- src/routes/signUpPage/SignUpPage.jsx
+++ src/routes/signUpPage/SignUpPage.jsx
@@ -4,7 +4,7 @@ import { SignUp } from '@clerk/clerk-react'
 const SignUpPage = () => {
   return (
     <div className='signUpPage'>
-      <SignUp path="/Machar-AI/sign-up" signInUrl="/Machar-AI/sign-in" />
+      <SignUp path="/Machar-AI/sign-up" signInUrl="/Machar-AI/sign-in" forceRedirectUrl="/Machar-AI/dashboard/" />
     </div>
   )
 }
