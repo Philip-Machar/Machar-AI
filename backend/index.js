199a51d7bf69fb89081edec61af644507226972e
Philip-Machar <philipmachar788@gmail.com>
2024-09-09 10:19:18 +0300

    Final commit

--- backend/index.js
+++ backend/index.js
@@ -15,10 +15,10 @@ const __dirname = path.dirname(__filename);
 
 
 app.use(cors({
-  origin: process.env.CLIENT_URL,
-  methods: ["GET", "POST"],
+  origin: ['https://machar-ai.vercel.app', 'https://machar-ai-main.vercel.app'],
   credentials: true
 }));
+
 
 app.use(express.json())
 
